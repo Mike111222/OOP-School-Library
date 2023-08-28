686d15c20af2165438d064bab98a5b1a6a8fa0bc
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-08-28 18:23:21 +0500

    Create teacher.rb

---
+++ teacher.rb
@@ -0,0 +1,10 @@
+class Teacher < Person
+  def initialize(id, age, specialization, parent_permission: true, name: 'Unknown')
+    super(id, age, parent_permission: parent_permission, name: name)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
