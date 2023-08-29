757134686d640bd3a79771897e371f7392ba4724
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-08-29 17:50:46 +0500

    Create decorator.rb

---
+++ decorator.rb
@@ -0,0 +1,25 @@
+require './nameable'
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super() # This is important to call the initialize method of the parent class
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    name = @nameable.correct_name
+    name.length > 10 ? name[0..9] : name
+  end
+end
