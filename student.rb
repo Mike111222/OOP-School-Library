48892284aa4540023d78802f40eb9ee6b6b2c8ad
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-08-30 18:39:31 +0500

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,19 +1,18 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
   attr_reader :classroom
 
-  def initialize(id, age, name = 'Unknown', parent_permission: true, classroom: nil)
-    super(id, age, name, parent_permission)
-    @classroom = classroom
-    add_student_to_classroom if classroom
-  end
-
-  def add_student_to_classroom
-    classroom.add_student(self) unless classroom.students.include?(self)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
   end
 
   def play_hooky
-    '¯(ツ)/¯'
+    '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
