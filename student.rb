f24f6e1c69cbe797ae800a68b33ccdde87a4fa55
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-08-30 17:16:37 +0500

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,7 +1,16 @@
+require './person'
+
 class Student < Person
-  def initialize(id, age, classroom, parent_permission: true, name: 'Unknown')
-    super(id, age, parent_permission: parent_permission, name: name)
+  attr_reader :classroom
+
+  def initialize(id, age, name = 'Unknown', parent_permission: true, classroom: nil)
+    super(id, age, name, parent_permission)
     @classroom = classroom
+    add_student_to_classroom if classroom
+  end
+
+  def add_student_to_classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
