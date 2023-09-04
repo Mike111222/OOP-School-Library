b402bbe952ea83a46709d080dd3e5ee7f46bfcbc
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-04 21:22:31 +0500

    Update student.rb

--- student.rb
+++ student.rb
@@ -15,4 +15,8 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
+
+  def to_s
+    "[Student] Name: #{@name}, ID: #{@id}, Age: #{@age}"
+  end
 end
