f0551c9d485f4f05d5859d00dd160004683fc00e
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-04 18:12:32 +0500

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_reader :specialization
+  attr_accessor :specialization
 
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
@@ -10,5 +10,19 @@ class Teacher < Person
 
   def can_use_services?
     true
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'age' => @age,
+      'specialization' => @specialization,
+      'name' => @name,
+      'parent_permission' => @parent_permission
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['age'], object['specialization'], object['name'], parent_permission: object['parent_permission'])
   end
 end
