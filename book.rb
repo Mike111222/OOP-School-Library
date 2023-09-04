4fe7747e50a334c6a9703efe8a72f3d7ddb6db42
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-04 18:07:49 +0500

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,4 +1,5 @@
 require_relative 'rental'
+require 'json'
 
 class Book
   attr_accessor :title, :author, :rentals
@@ -12,5 +13,17 @@ class Book
   def add_rental(rental)
     @rentals.push(rental)
     rental.book = self
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'title' => @title,
+      'author' => @author
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    new(object['title'], object['author'])
   end
 end
