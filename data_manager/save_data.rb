f85f9c89f6014575d1f6ea0f050e5c94f00b0c43
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-05 14:49:45 +0500

    Update save_data.rb

--- data_manager/save_data.rb
+++ data_manager/save_data.rb
@@ -4,11 +4,9 @@ require 'fileutils'
 class SaveData
   def self.save_data_to_json(file_name, data)
     folder_path = File.dirname(file_name)
-    
+
     FileUtils.mkdir_p(folder_path) unless File.directory?(folder_path)
 
-    File.open(file_name, 'w') do |file|
-      file.write(JSON.generate(data))
-    end
+    File.write(file_name, JSON.generate(data))
   end
 end
