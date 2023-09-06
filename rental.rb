3a7e462f0d6be022d794164829adbad7ccda9d60
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-06 17:08:51 +0500

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,3 +1,6 @@
+require 'json'
+require_relative 'book'
+require_relative 'person'
 class Rental
   attr_accessor :date, :book, :person
 
@@ -11,5 +14,22 @@ class Rental
 
   def to_s
     "Date: #{@date}, Book: \"#{@book.title}\" by #{@book.author}"
+  end
+
+  def to_json(*_args)
+    JSON.dump({
+                date: @date,
+                book_id: @book.id,
+                person_id: @person.id
+              })
+  end
+
+  def self.from_json(string, books, persons)
+    data = JSON.parse string
+    book_id = data['book_id']
+    person_id = data['person_id']
+    book = Book.select(book_id, books)
+    person = Person.select(person_id, persons)
+    new(data['date'], book, person)
   end
 end
