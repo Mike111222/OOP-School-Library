cffbb6305b0ba9057cefb8d1bc366b4b91fb3923
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-06 21:55:32 +0500

    Create spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require_relative '../book'
+require_relative '../person'
+require_relative '../student'
+require_relative '../decorator'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
